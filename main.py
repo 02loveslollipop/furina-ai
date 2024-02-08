ef48febfab82f33425a1d852eb9093b0bfb8561d
02loveslollipop <mmonsalve2005@gmail.com>
2024-02-07 23:13:35 -0500

    Update main.py: Add functionality to save conversation as CSV file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Inference import Inference
 from confLoader import ConfLoader
 import time
+import pandas as pd
 
 if __name__ == "__main__":
     conf = ConfLoader()
@@ -13,13 +14,34 @@ if __name__ == "__main__":
         print("prompt.txt not found")
         prompt = "You are a helpful assistant that answer questions"
     
-    ai_model = Inference(load_in_4bit=False, load_in_8bit=True, huggingface_token=conf.token, model_name=conf.model, device=conf.device, torch_dtype="auto",prompt=prompt)
+    ai_model = Inference(load_in_4bit=True, load_in_8bit=False, huggingface_token=conf.token, model_name=conf.model, device=conf.device, torch_dtype="auto",prompt=prompt)
     
     next = True
     
     while next:
         prompt = input("Enter your question: ")
         now = time.time()
-        print(ai_model.generate(prompt))
+        print(ai_model.generate(prompt,max_new_tokens=650))
         print("generated in: ", time.time() - now)
         next = input("Do you want to continue? (y/n): ").lower() == "y"
+        if next:
+            print(f"current batch: {ai_model.messages}")
+            input("Press enter to continue")
+        else:
+            save = input("Do you want to save the conversation? (y/n): ").lower() == "y"
+            if save:
+                name = input("Enter the name of the file: (without extension): ")
+                df = pd.DataFrame(ai_model.messages)
+                saved = False
+                while not saved:
+                    try:
+                        df.to_csv(f"{name}.csv",index=False)
+                        print(f"conversation saved as {name}.csv")
+                        saved = True
+                    except PermissionError:
+                        request = input("PermissionError: the file is already open, do you want to try again? (y/n): ")
+                        if request.lower() == "n":
+                            saved = True
+                        
+                    
+                
