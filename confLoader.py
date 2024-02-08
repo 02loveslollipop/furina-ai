2b7dfccd4ee7c9266982c49e888012e5629f084a
02loveslollipop <mmonsalve2005@gmail.com>
2024-02-08 11:41:10 -0500

    Update configuration file with additional settings and values

--- confLoader.py
+++ confLoader.py
@@ -12,9 +12,21 @@ class ConfLoader:
         finally:
             try:
                 config = yaml.load(config_file, Loader=yaml.FullLoader)
-                self.token = config['token']
-                self.model = config['model']
-                self.device = config['device']
+                self.token = config['model']['token']
+                self.model = config['model']['model']
+                self.device = config['model']['device']
+                self.max_new_tokens = config['model']['max_new_tokens']
+                self.temperature = config['model']['temperature']
+                self.top_k = config['model']['top_k']
+                self.top_p = config['model']['top_p']
+                self.do_sample = config['model']['do_sample']
+                self.torch_dtype = config['model']['torch_dtype']
+                self.load_in_4bit = config['quantization']['load_in_4bit']
+                self.load_in_8bit = config['quantization']['load_in_8bit']
+                self.port = config['api']['port']   
+                self.host = config['api']['host']
+                self.debug = config['api']['debug']
+                self.api_token = config['api']['token']
             except KeyError:
                 print("Your config file is missing a required field.")
                 exit(1)
